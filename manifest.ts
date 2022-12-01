71e980977c09937d23ccbd595b03e435c5079a6e
Yoshiki Ozaki <giboshi31@gmail.com>
2022-12-01 20:12:54 +0900

    first commit

---
+++ manifest.ts
@@ -0,0 +1,19 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import { CreateTimeOffRequestWorkflow } from "./workflows/CreateTimeOffRequestWorkflow.ts";
+import { SendTimeOffRequestToManagerFunction } from "./functions/send_time_off_request_to_manager/definition.ts";
+
+export default Manifest({
+  name: "focused-gorilla-402",
+  description: "Ask your manager for some time off",
+  icon: "assets/default_new_app_icon.png",
+  workflows: [CreateTimeOffRequestWorkflow],
+  functions: [SendTimeOffRequestToManagerFunction],
+  outgoingDomains: [],
+  botScopes: [
+    "commands",
+    "chat:write",
+    "chat:write.public",
+    "datastore:read",
+    "datastore:write",
+  ],
+});
