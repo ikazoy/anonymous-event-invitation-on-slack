9c1be0db4f2d42d6b9f890980cc6395d53d6fb94
Yoshiki Ozaki <giboshi31@gmail.com>
2024-05-28 16:21:44 +0900

    add Trigger type definition

--- triggers/trigger.ts
+++ triggers/trigger.ts
@@ -1,15 +1,17 @@
 import { Trigger } from "deno-slack-api/types.ts";
+import { CreateEventInvitationWorkflow } from "../workflows/CreateEventInvitationWorkflow.ts";
 
-export const trigger: Trigger = {
-  type: "shortcut",
-  name: "ぺこりーの イベント作成",
-  description: "イベントを作成して、参加者を募集できます。",
-  workflow: "#/workflows/create_event",
-  inputs: {
-    interactivity: {
-      value: "{{data.interactivity}}",
+export const trigger: Trigger<typeof CreateEventInvitationWorkflow.definition> =
+  {
+    type: "shortcut",
+    name: "ぺこりーの イベント作成",
+    description: "イベントを作成して、参加者を募集できます。",
+    workflow: "#/workflows/create_event",
+    inputs: {
+      interactivity: {
+        value: "{{data.interactivity}}",
+      },
     },
-  },
-};
+  };
 
 export default trigger;
